758387d5c9da451229ebe6c35e36eaa02da9bbb5
Rafael Marinho <rafael_mf3@hotmail.com>
2019-10-11 10:30:03 -0300

    Update ReactotronConfig.js

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -2,6 +2,7 @@ import Reactotron, { asyncStorage } from 'reactotron-react-native';
 
 
 if (__DEV__) {
+  // HOST: SEU IP!
   const tron = Reactotron.configure({ host: '192.168.100.8' }).use(asyncStorage()).useReactNative().connect();
 
   console.tron = tron;
