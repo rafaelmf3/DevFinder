366157b4b8028ffed3b1fc2832767f3c70c23200
renatopcinfo <renatopcinfo@gmail.com>
2019-10-20 08:19:29 -0300

    styled-components screen Login

--- src/Components/Dev/index.js
+++ src/Components/Dev/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { withNavigation } from 'react-navigation';
-import { View, TouchableOpacity, Text } from 'react-native';
+import { View, TouchableOpacity, Text, Image } from 'react-native';
 
-function Dev({dev, navigation, user, city}) {
+function Dev({ dev, navigation, user, city }) {
   function handleDevProfile() {
     navigation.navigate('DevProfile', { dev, user, city });
   }
-
+  console.log(dev)
   return (
     <View>
       <TouchableOpacity onPress={handleDevProfile}>
-        <Text style={{fontSize: 20, margin: 20}}>{dev.login}</Text>
+        <Text style={{ fontSize: 20, margin: 20 }}>{dev.login}</Text>
       </TouchableOpacity>
 
     </View>
