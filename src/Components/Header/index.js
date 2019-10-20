5fc150b3951dd0d95d8132c30e4d0661da5a4c9c
renatopcinfo <renatopcinfo@gmail.com>
2019-10-20 09:39:05 -0300

    styled-components Header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { View, Text, Image, StyleSheet } from 'react-native'
+
+import { Container, ImgPerfil, TxtLocation, ImgLogo } from './styles';
 
 
 type HeaderProps = {
@@ -10,34 +11,11 @@ type HeaderProps = {
 export const Header = (props: HeaderProps) => {
   const { image = "https://img.olhardigital.com.br/uploads/acervo_imagens/2014/12/r16x9/20141204130826_1200_675_-_perfil_facebook.jpg", city } = props
   return (
-    <View style={Styles.container}>
-      <Image style={Styles.imagePerfil} source={{ uri: image }} />
-      <Text style={Styles.textLocation}>{city ? city : "Não Encontrado"}</Text>
-      <Image style={Styles.imageLogo} resizeMode={'contain'} source={require('./../../../assets/logo.png')} />
-    </View>
+    <Container>
+      <ImgPerfil source={{ uri: image }} />
+      <TxtLocation >{city ? city : "Não Encontrado"}</TxtLocation>
+      <ImgLogo resizeMode={'contain'} source={require('./../../../assets/logo.png')} />
+    </Container>
   )
 }
 
-const Styles = StyleSheet.create({
-  container: {
-    paddingTop: 30,
-    paddingBottom: 10,
-    paddingHorizontal: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: '#191970'
-  },
-  imagePerfil: {
-    width: 50,
-    height: 50,
-    borderRadius: 50 / 2
-  },
-  imageLogo: {
-    height: 50,
-    width: 60
-  },
-  textLocation: {
-    color: 'white'
-  }
-})
